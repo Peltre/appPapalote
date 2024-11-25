fe2febb0c59171063480feaf15c8efc730d2050c
Carlos Saldaña <A01285600@tec.mx>
2024-11-25 00:27:03 -0600

    agregado boton para cerrar vista de noticias a inicio

--- proyectoReto/Inicio/vistaNoticias.swift
+++ proyectoReto/Inicio/vistaNoticias.swift
@@ -1,20 +1,36 @@
 //
-//  vistaNoticias.swift
-//  proyectoReto
+// vistaNoticias.swift
+// proyectoReto
 //
-//  Created by Pedr1p on 05/11/24.
+// Created by Pedr1p on 05/11/24.
 //
 
 import SwiftUI
 
 struct vistaNoticias: View {
+    @Environment(\.presentationMode) var presentationMode // Ambiente para controlar la navegación
+
     var body: some View {
-        // Pasar la URL de la página de Facebook al WebView
         WebView(url: URL(string: "https://www.facebook.com/PapaloteMuseoMty/")!)
-            .edgesIgnoringSafeArea(.all)  // Opcional, para mostrar en pantalla completa
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarTitle("Noticias", displayMode: .inline)
+            .navigationBarBackButtonHidden(true) // Esconde el botón de retroceso predeterminado
+            .navigationBarItems(leading: Button(action: {
+                presentationMode.wrappedValue.dismiss() // Cierra la vista y regresa
+            }) {
+                Image(systemName: "arrow.left")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 25)
+                    .foregroundColor(.black)
+                    .padding(10)
+                    .background(Color(white: 1))
+                    .clipShape(Circle())
+                    .shadow(radius: 3, y: 2)
+                    .foregroundColor(.blue)
+            })
     }
 }
-
 
 #Preview {
     vistaNoticias()
